02635d7723c3e106b36c55d1563e85bc7704b451
Reuben Rissler <benreu.rissler@gmail.com>
2025-04-03 09:47:04 -0400

    velocity PWM out was not working
    fix uncompleted arduino-espressif V2 to V3 updates
    use GPIO pin instead of channel number
    PWM resolution less than 10 bits does not output PWM
    do not use more than 2 decimal places in floats -> can crash ESP32
    remove unneeded pinMode

--- AOG_GPS_ESP32/src/speedPwmOut.cpp
+++ AOG_GPS_ESP32/src/speedPwmOut.cpp
@@ -10,16 +10,16 @@ void SpeedPWM ( void* z ){
   for( ;; ) {
 
     mphPwm = float( UBXPVT1[UBXRingCount1].gSpeed ) * gpsConfig.velocityHzPerMPH;
-    mphPwm *= 0.0022369363; // 1 gSpeed = 0.0036 km/h = 0.0022369363 mile/h 
-    ledcWriteTone( 0, mphPwm );
+    mphPwm *= 0.22; // 1 gSpeed = 0.0036 km/h = 0.0022369363 mile/h 
+    mphPwm /= 100;
+    ledcWriteTone( gpsConfig.gpioVelocityPWM, mphPwm );
 		vTaskDelayUntil( &xLastWakeTime, xFrequency );
   }
 }
 
 void initSpeedPWM() {
 
-  pinMode( ( uint8_t ) gpsConfig.gpioVelocityPWM, OUTPUT );
-  ledcAttach( ( uint8_t ) gpsConfig.gpioVelocityPWM, gpsConfig.velocityHzPerMPH, 8 );
-  ledcWrite( 0, 128 );
+  ledcAttach( gpsConfig.gpioVelocityPWM, gpsConfig.velocityHzPerMPH, LEDC_TIMER_10_BIT );
+  ledcWrite( gpsConfig.gpioVelocityPWM, 512 ); // 50% duty cycle
   xTaskCreate( SpeedPWM, "SpeedPWM", 2048, NULL, 2, NULL );
 }
